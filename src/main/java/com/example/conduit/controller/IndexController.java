e2d02943f98bd51a396ed01a38d01ea18980cd74
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-05-09 23:45:45 +0900

    htmx

--- src/main/java/com/example/conduit/controller/IndexController.java
+++ src/main/java/com/example/conduit/controller/IndexController.java
@@ -1,76 +1,16 @@
 package com.example.conduit.controller;
 
-import com.example.conduit.controller.article.ArticlePreviewDTO;
-import com.example.conduit.controller.profile.ProfileDTO;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Controller
 public class IndexController {
 
     @GetMapping
-    public String index(Model model) throws URISyntaxException, MalformedURLException {
-        model.addAttribute("articleList", List.of(
-                new ArticlePreviewDTO(
-                        "how-to-train-your-dragon",
-                        "Ever wonder how?",
-                        "You have to believe",
-                        "body",
-                        List.of("training", "dragons"),
-                        LocalDateTime.now(),
-                        LocalDateTime.now(),
-                        false,
-                        29,
-                        new ProfileDTO(
-                                "Eric Simons",
-                                null,
-                                new URI("http://i.imgur.com/Qr71crq.jpg").toURL(),
-                                false
-                        )
-                ),
-                new ArticlePreviewDTO(
-                        "how-to-train-your-dragon",
-                        "Ever wonder how?",
-                        "You have to believe",
-                        "body",
-                        List.of("training", "dragons"),
-                        LocalDateTime.now(),
-                        LocalDateTime.now(),
-                        false,
-                        29,
-                        new ProfileDTO(
-                                "Eric Simons",
-                                null,
-                                new URI("http://i.imgur.com/Qr71crq.jpg").toURL(),
-                                false
-                        )
-                ),
-                new ArticlePreviewDTO(
-                        "how-to-train-your-dragon",
-                        "Ever wonder how?",
-                        "You have to believe",
-                        "body",
-                        List.of("training", "dragons"),
-                        LocalDateTime.now(),
-                        LocalDateTime.now(),
-                        false,
-                        29,
-                        new ProfileDTO(
-                                "Eric Simons",
-                                null,
-                                new URI("http://i.imgur.com/Qr71crq.jpg").toURL(),
-                                false
-                        )
-                )
-        ));
+    public String index() throws URISyntaxException, MalformedURLException {
         return "index";
     }
 
--- src/main/java/com/example/conduit/controller/article/ArticleController.java
+++ src/main/java/com/example/conduit/controller/article/ArticleController.java
@@ -1,9 +1,17 @@
 package com.example.conduit.controller.article;
 
+import com.example.conduit.controller.profile.ProfileDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 @RequestMapping("/articles")
@@ -12,5 +20,64 @@ public class ArticleController {
     @GetMapping("/{slug}")
     public String article(@PathVariable("slug") String slug) {
         return "article";
+    }
+
+    @GetMapping
+    public String list(Model model) throws URISyntaxException, MalformedURLException {
+        model.addAttribute("articleList", List.of(
+                new ArticlePreviewDTO(
+                        "how-to-train-your-dragon",
+                        "Ever wonder how?",
+                        "You have to believe",
+                        "body",
+                        List.of("training", "dragons"),
+                        LocalDateTime.now(),
+                        LocalDateTime.now(),
+                        false,
+                        29,
+                        new ProfileDTO(
+                                "Eric Simons",
+                                null,
+                                new URI("http://i.imgur.com/Qr71crq.jpg").toURL(),
+                                false
+                        )
+                ),
+                new ArticlePreviewDTO(
+                        "how-to-train-your-dragon",
+                        "Ever wonder how?",
+                        "You have to believe",
+                        "body",
+                        List.of("training", "dragons"),
+                        LocalDateTime.now(),
+                        LocalDateTime.now(),
+                        false,
+                        29,
+                        new ProfileDTO(
+                                "Eric Simons",
+                                null,
+                                new URI("http://i.imgur.com/Qr71crq.jpg").toURL(),
+                                false
+                        )
+                ),
+                new ArticlePreviewDTO(
+                        "how-to-train-your-dragon",
+                        "Ever wonder how?",
+                        "You have to believe",
+                        "body",
+                        List.of("training", "dragons"),
+                        LocalDateTime.now(),
+                        LocalDateTime.now(),
+                        false,
+                        29,
+                        new ProfileDTO(
+                                "Eric Simons",
+                                null,
+                                new URI("http://i.imgur.com/Qr71crq.jpg").toURL(),
+                                false
+                        )
+                )
+        ));
+
+        return "index :: article-preview";
     }
 }
