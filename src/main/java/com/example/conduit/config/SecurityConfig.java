f17ae813ad5e384e4047b8aef0702a32724b3131
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-05-09 22:24:31 +0900

    login

--- src/main/java/com/example/conduit/config/SecurityConfig.java
+++ src/main/java/com/example/conduit/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.example.conduit.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -12,9 +15,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                .anyRequest().permitAll()
-        );
+        http
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .anyRequest().permitAll()
+                )
+                .formLogin(formLogin -> formLogin
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .loginPage("/login").permitAll()
+                )
+        ;
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
--- src/main/java/com/example/conduit/controller/IndexController.java
+++ src/main/java/com/example/conduit/controller/IndexController.java
@@ -5,6 +5,7 @@ import com.example.conduit.controller.profile.ProfileDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.net.MalformedURLException;
 import java.net.URI;
