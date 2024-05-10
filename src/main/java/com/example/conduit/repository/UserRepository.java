859ef30a3eaabaf95f10a755c0943bddd96c1603
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-05-10 10:42:15 +0900

    login by connection to database

--- src/main/java/com/example/conduit/repository/UserRepository.java
+++ src/main/java/com/example/conduit/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.conduit.repository;
 import com.example.conduit.service.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/example/conduit/security/CustomUserDetailsService.java
+++ src/main/java/com/example/conduit/security/CustomUserDetailsService.java
@@ -1,19 +1,31 @@
 package com.example.conduit.security;
 
-import org.springframework.security.core.userdetails.User;
+import com.example.conduit.repository.UserRepository;
+import com.example.conduit.service.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return User.builder()
-                .username(username)
-                .password("{noop}password")
-                .roles("USER")
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(this::toUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    private UserDetails toUserDetails(User user) {
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
                 .build();
     }
 }
